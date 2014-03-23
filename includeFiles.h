2a92623d44955d304e66d2215fe31fdfc39c68c6
Rohan D. Shah <rohan.shah3@aggiemail.usu.edu>
2014-03-23 02:00:46 -0600

    contains all the includes
    contains the list of header files that needs to be included in the main
    and other respective files

---
+++ includeFiles.h
@@ -0,0 +1,14 @@
+#include "World.h"
+
+#include "Grid.h"
+#include "GridLocation.h"
+
+#include "Actor.h"
+#include "Player.h"
+#include "Warp.h"
+#include "Wall.h"
+#include "Bomb.h"
+#include "Box.h"
+#include "Exit.h"
+#include "EmptySpace.h"
+#include "Hostage.h"
