840cdccd0540a57ca02976bb8e0763c2388341f3
Rohan D. Shah <rohan.shah3@aggiemail.usu.edu>
2014-03-23 02:01:01 -0600

    contains the libraries that needs to be included

---
+++ libraries.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdlib.h> 
+#include <fstream>
+#include <string.h>
+#include <stdio.h>
+#include <stack>
+#include <vector>
+#include <cstring>
+#include <cstdlib>
+#include <cerrno>
+#include <dirent.h>
+#include <sys/stat.h>
